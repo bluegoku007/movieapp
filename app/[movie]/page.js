fab7aac2604dc2b9f0a96b5f881450819bdf856d
EWDyKtd1uQ <selmenselmi5@gmail.com>
2023-03-08 21:18:30 +0100

    rez

--- app/[movie]/page.js
+++ app/[movie]/page.js
@@ -1,24 +1,45 @@
 import Image from "next/image"
-export  async function generateStaticParams() {
-    const data= await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`,{next: {revalidate:60}})
-    const res=await data.json()
-    return res.results.map((movie)=>({
-        movie: toString(movie.id),
-    }))
+
+export async function generateStaticParams() {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  )
+  const res = await data.json()
+
+  return res.results.map((movie) => ({
+    movie: toString(movie.id),
+  }))
 }
-export default async function MovieDetails({params}){
-    const {movie} =params
-    const imagePath = "https://image.tmdb.org/t/p/original"
-    const data= await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`)
-   const res = await data.json()
-    return(
-        <div>
-            <h1 className="text-2x1">{res.title}</h1>
-            <h1 className="text-2x1">{res.release_date}</h1>
-            <h1 className="text-2x1">{res.runtime} minutes</h1>
-            <h1 className="bg-green-600 inline-block my-2 py-2 px-4 rounded-medium">{res.status}</h1>
-            <Image className="my-12 w-full" src={imagePath + res.backdrop_path} width={1000} height={1000} />
-        <p>{res.overview}</p>
-        </div>
-    )
+
+export default async function MovieDetail({ params }) {
+  const { movie } = params
+  const imagePath = "https://image.tmdb.org/t/p/original"
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`
+  )
+  const res = await data.json()
+
+  return (
+    <div>
+      <div className="">
+        <h2 className="text-4xl">{res.title}</h2>
+        <h1 className="text-lg ">{res.release_date}</h1>
+        <h2>Runtime: {res.runtime} minutes</h2>
+        <h2 className="bg-green-600 inline-block my-2 py-2 px-4 rounded-lg text-sm">
+          {res.status}
+        </h2>
+        <Image
+          className="my-12 w-full"
+          src={imagePath + res.backdrop_path}
+          alt={res.title}
+          width={1000}
+          height={1000}
+          priority
+        />
+      </div>
+      <div className="my-4">
+        <p className="text-lg">{res.overview}</p>
+      </div>
+    </div>
+  )
 }
